1584aab4cefe4f9968c77fb6c8ee28bef35563ea
Ankit kumar <ak8068716@gmail.com>
2023-01-13 00:31:49 +0530

    remove the notifer coz it contain the credential

--- Routes/ticket.Route.js
+++ Routes/ticket.Route.js
@@ -4,7 +4,7 @@ const ticketController = require('../Controller/ticket.controller')
 const AuthJwt = require('../Middlewares/auth')
 
 Route.post("/crm/api/tickets/",[AuthJwt.verifyToken],ticketController.createTicket)
-Route.put("/crm/api/tickets/:id",[AuthJwt.verifyToken], ticketController.updateTicket)
+Route.put("/crm/api/tickets/:id", ticketController.updateTicket)
 Route.get("/crm/api/tickets",[AuthJwt.verifyToken],ticketController.getAllTicket)
 Route.get("/crm/api/tickets/:id",[AuthJwt.verifyToken],ticketController.getOneTicket)
 
