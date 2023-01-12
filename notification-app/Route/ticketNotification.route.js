347c261d69c8e0ae3083f523bf8e46462a2fc316
Ankit kumar <ak8068716@gmail.com>
2023-01-13 00:42:23 +0530

    notification deleted

--- notification-app/Route/ticketNotification.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const notificationController = require('../controllers/ticketNotification.controller')
-
-route.post("/notifiServ/api/notifications/",notificationController.acceptNotificationRequest)
-route.get("/notifiServ/api/notifications/:id",notificationController.getNotificationStatus)
-
-module.exports = route
--- notification-app/configs/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DB_NAME: "notification_db",
-    DB_URL: "mongodb://localhost:27017/notification_db"
-}
